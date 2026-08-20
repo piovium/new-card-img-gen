6b64fa8db4add00e2c0c84840ab9105866ca7258
guyutongxue <guyutongxue@163.com>
2026-08-20 23:59:49 +0800

    introduce assets-manager

--- src/shared.ts
+++ src/shared.ts
@@ -1,37 +1,53 @@
-import type { AllRawData, Language } from "./types.ts";
+import {
+  AssetsManager,
+  DEFAULT_ASSETS_API_ENDPOINT,
+} from "@gi-tcg/assets-manager";
+import type { AllRawData, Language, Version } from "./types.ts";
 import {
   normalizeCodeAnalyzerResults,
   type CodeAnalyzerResult,
 } from "./codeAnalyzer.ts";
 
-export const ASSETS_API_ENDPOINT = import.meta.env?.ASSETS_API_ENDPOINT || `https://static-data.piovium.org/api/v4`;
 // @ts-expect-error Node types
 const runtimeEnv = globalThis.process?.env;
+
+export const ASSETS_API_ENDPOINT =
+  import.meta.env?.ASSETS_API_ENDPOINT ||
+  runtimeEnv?.ASSETS_API_ENDPOINT ||
+  DEFAULT_ASSETS_API_ENDPOINT;
 
 export const DATA_CODE_ANALYZER_RESULT_ENDPOINT =
   import.meta.env?.DATA_CODE_ANALYZER_RESULT_ENDPOINT ||
   runtimeEnv?.DATA_CODE_ANALYZER_RESULT_ENDPOINT ||
   "https://play.piovium.org/api/data_code_analyzer_result";
 
-export const getData = async (version: string, language: Language) => {
-  const data: Partial<AllRawData> = {};
-  await Promise.all(
-    (["characters", "action_cards", "entities", "keywords"] as const).map(
-      async (category) => {
-        const key = category === "action_cards" ? "actionCards" : category;
-        data[key] = await fetch(
-          `${ASSETS_API_ENDPOINT}/data/${version}/${language}/${category}`
-        ).then(async (r) =>
-          r.ok
-            ? (
-                await r.json()
-              ).data
-            : Promise.reject(new Error(await r.text()))
-        );
-      }
-    )
-  );
-  return data as AllRawData;
+const assetsManagers = new Map<string, AssetsManager>();
+
+export const getAssetsManager = (version: Version, language: Language) => {
+  let manager = assetsManagers.get(`${version}.${language}`);
+  if (!manager) {
+    manager = new AssetsManager({
+      apiEndpoint: ASSETS_API_ENDPOINT,
+      version,
+      language,
+    });
+    assetsManagers.set(language, manager);
+  }
+  return manager;
+};
+
+export const getData = async (
+  version: Version,
+  language: Language,
+): Promise<AllRawData> => {
+  const manager = getAssetsManager(version, language);
+  const [characters, actionCards, entities, keywords] = await Promise.all([
+    manager.getCategory("characters"),
+    manager.getCategory("action_cards"),
+    manager.getCategory("entities"),
+    manager.getCategory("keywords"),
+  ]);
+  return { characters, actionCards, entities, keywords };
 };
 
 export const getCodeAnalyzerResults = async (): Promise<
